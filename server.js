fcb4a1a6f0de6caddcc91d85e1ffcff9422b3669
vivekmit05 <vivek.mit05@gmail.com>
2017-09-10 22:04:10 +0530

    commited locally

--- server.js
+++ server.js
@@ -5,33 +5,88 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+title:'Article-one | Vivek Mishra',
+heading: 'Article One',
+date: 'Sep, 10, 2017',
+content:`
+            <p>
+                 This is 1st paragraph of article one.
+             </p>
+             <p>
+                 This is 2nd paragraph of article one.
+             </p>`
+};
+
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+
+var htmlTemplate=`
+    <!doctype html>
+    <html>
+      <head>
+        <title>$(title)</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link href="/ui/style.css" rel="stylesheet"/>
+      </head>
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+                </div>
+                <hr>
+                <h3>
+                    $(heading)
+                </h3>
+                <div>
+                    $(date)
+                </div>
+             <div>
+                 $(content)
+             </div>
+          </div>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
+
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/index.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+// app.get('/index.html', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+// });
+
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+  res.send(createTemplate(articleOne));
 });
-app.get('/article-one.html',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
-});
+// app.get('/article-one',function(req,res){
+//     res.sendFile(path.join(__dirname,'ui','article-one.html'));
+// });
+// app.get('/article-one.html',function(req,res){
+//     res.sendFile(path.join(__dirname,'ui','article-one.html'));
+// });
 
 app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
-app.get('/article-two.html',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
-});
+// app.get('/article-two.html',function(req,res){
+//     res.sendFile(path.join(__dirname,'ui','article-two.html'));
+// });
 
 app.get('/article-three',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
-app.get('/article-three.html',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-three.html'));
-});
+// app.get('/article-three.html',function(req,res){
+//     res.sendFile(path.join(__dirname,'ui','article-three.html'));
+// });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
