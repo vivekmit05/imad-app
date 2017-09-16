594ee46f683ea442b542f8783b074498ac50506d
vivekmit05 <31102746+vivekmit05@users.noreply.github.com>
2017-09-16 11:58:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
 
+var config={
+    user:'vivekmit05',
+    database:'vivekmit05',
+    host:'http://db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
+
 
 var articles={
   'article-one':{
@@ -80,6 +89,20 @@ var htmlTemplate=`
     return htmlTemplate;
 
 }
+
+var pool=new Pool(config);
+app.get('/testDB',function(req,res){
+    //make a select request and return the response with the result
+    
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 var counter=0;
 app.get('/counter',function(req,res){
