e60ccb4bc5732e4b04cedaa0cf9ffef7cad46ec2
vivekmit05 <31102746+vivekmit05@users.noreply.github.com>
2017-09-16 12:02:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/testDB',function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
