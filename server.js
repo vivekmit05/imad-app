f55d7b7d26ea1f8e8b6635a7f29ba81a666f8954
vivekmit05 <31102746+vivekmit05@users.noreply.github.com>
2017-09-19 01:11:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get('/ui/main.js',function(req,res){
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input, salt, 10000, 10, 'sha512');
-    return ['pdkdf2','salt','10000',hashed.toString('hex')].join('&');
+    return ['pdkdf2','salt','10000',hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input,'this-is-a-salt-value');
