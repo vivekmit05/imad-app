d38f3b1c7f417feda85390bacd967c941650520a
vivekmit05 <31102746+vivekmit05@users.noreply.github.com>
2017-09-19 01:30:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -170,9 +170,25 @@ function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input, salt, 10000, 10, 'sha512');
     return ['pdkdf2','salt','10000',hashed.toString('hex')].join('$');
 }
+
+//End-point to test hash function
 app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input,'this-is-a-salt-value');
     res.send(hashedString);
+});
+
+//End-point to create User
+app.post('/create-user',function(req,res){
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(username+' user created successfully');
+        }
+    });
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
