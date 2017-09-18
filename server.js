1cb4dc3ff27ebed242b2dfa4b77b29d58640be18
vivekmit05 <31102746+vivekmit05@users.noreply.github.com>
2017-09-19 01:38:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser=require('body-parser');
 
 var config={
     user:'vivekmit05',
@@ -15,6 +16,7 @@ var pool=new Pool(config);//created pool globally.
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());//Asking "express app" that in case you see JSON content load that JSON content into "req.body" variable
 
 //Article objects used to create template
 var articles={
@@ -179,7 +181,9 @@ app.get('/hash/:input',function(req,res){
 
 //End-point to create User
 app.post('/create-user',function(req,res){
-    var salt=crypto.getRandomBytes(128).toString('hex');
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
     var dbString=hash(password,salt);
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
