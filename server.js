4a5700e0d0ce8c7c670feabb4b24d45fa18adeee
vivekmit05 <31102746+vivekmit05@users.noreply.github.com>
2017-09-19 02:17:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ app.post('/create-user',function(req,res){
     var dbString=hash(password,salt);
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
-            res.status(500).send('In the err block');
+            res.status(500).send(err.toString());
         }
         else{
             res.send(username+' user created successfully');
